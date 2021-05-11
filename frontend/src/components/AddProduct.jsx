138a6f30a3dcd45790269baf5ab5ceb3820f7abd
Jeremy <jeremy@maans.eu>
2021-05-11 05:20:46 +0200

    delete database product.sqlite

---
+++ frontend/src/components/AddProduct.jsx
@@ -0,0 +1,59 @@
+import { useState, useRef } from "react";
+
+
+function AddProduct({ CreateProduct }) {
+  const [open, setOpen] = useState(false);
+  const id = useRef(false);
+  const name = useRef(false);
+  const city = useRef(false);
+  const price = useRef(false);
+
+  function Cancel() {
+    id.current.value = "";
+    name.current.value = "";
+    city.current.value = "";
+    price.current.value = "";
+    setOpen(false);
+  }
+  function Valid() {
+    CreateProduct(id, name, city, price, Cancel);
+  }
+
+  return (
+    <div className="addProduct">
+      <div
+        className={open ? "popover open" : "popover close"}
+        onClick={(event) => {
+          if (event.target.classList.contains("popover")) {
+            setOpen(!open);
+          }
+        }}
+      >
+        <div className="form">
+          <div className="title">Créer un Produit</div>
+          <label htmlFor="id">Ref</label>
+          <input type="text" ref={id} name="id" />
+          <label htmlFor="name">Nom Produit</label>
+          <input type="text" ref={name} name="name" />
+          <label htmlFor="city">Ville</label>
+          <input type="text" ref={city} name="city" />
+          <label htmlFor="price">Prix</label>
+          <input type="number" ref={price} name="price" />
+          <div className="buttons">
+            <button onClick={Valid}>Valider</button>
+            <button onClick={Cancel}>Annuler</button>
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default AddProduct;
